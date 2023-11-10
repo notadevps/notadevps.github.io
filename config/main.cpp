a3b392adf2189bf54ebabf62f73546e61412732a
sahil <sahilarora8527397612@gmail.com>
2023-11-10 18:36:34 +0530

    3.0

---
+++ config/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define TRUE 1 
+#define FALSE 0
+
+
+const int BASIC_PROJECT = FALSE; 
+
+int main(int argc, char **argv) {
+    if (argc > 2) BASIC_PROJECT = FALSE; 
+
+    
+
+    
+    return 0; 
+}
